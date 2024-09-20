2f2e6284976562ea0e870d3bd49bec611cdf1c3a
AdrianGzn <233318@ids.upchiapas.edu.mx>
2024-09-20 12:02:50 -0500

    form

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,32 +2,34 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { StudentService, Student } from '../../services/student.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, ReactiveFormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
-  constructor (private studentService: StudentService) {}
-
   public students:Student[] = [];
+  myForm: FormGroup;
+
+  constructor (private studentService: StudentService, private formB: FormBuilder) {
+    this.myForm = this.formB.group({
+      name: ['', [Validators.minLength(1), Validators.maxLength(40)]],
+      lastName: ['', [Validators.minLength(1), Validators.maxLength(40)]],
+      age: [null]
+    })
+  }
 
   receiveMessage($event: String){
     alert($event)
   }
 
   addStudent() {
-    console.log("Agregado");
-    let student:Student = {
-      name: "Adrián",
-      lastName: "Guzmán",
-      age: 19
-    }
-    if(this.studentService.addStudent(student)){
+    const stundet = this.myForm.value;
+    if(this.studentService.addStudent(stundet)){
       console.log("Agregado")
     }
   }
@@ -37,6 +39,5 @@ export class MainComponent {
     console.log(this.students);
     
     console.log("Actualizado");
-    
   }
 }
